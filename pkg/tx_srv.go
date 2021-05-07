03d8fd7b6096f8ed458b4cc3d440070529aa4e40
Mikhail Klyushin <klyuhsinmisha@gmail.com>
2021-05-07 03:52:42 +0300

    change formatting for help message

--- pkg/tx_srv.go
+++ pkg/tx_srv.go
@@ -10,14 +10,14 @@ import (
 
 const helpSplash = `Commands structure:
 Data manipulation commands:
-	GET key         - finds value associated with key
-	SET key value   - sets value associated with key
-	DEL key         - removes value associated with key
+    GET key         - finds value associated with key
+    SET key value   - sets value associated with key
+    DEL key         - removes value associated with key
 Transaction management commands:
-	BEGIN SHARED    - starts new transaction with per-operation isolation
-	BEGIN EXCLUSIVE - starts new transaction with per-transation isolation
-	COMMIT          - commits active transaction
-	ABORT           - aborts active transaction
+    BEGIN SHARED    - starts new transaction with per-operation isolation
+    BEGIN EXCLUSIVE - starts new transaction with per-transation isolation
+    COMMIT          - commits active transaction
+    ABORT           - aborts active transaction
 `
 
 const (
