f95e055f9e8713856079eb41dfa3dc2ed48c1774
Mikhail Klyushin <klyuhsinmisha@gmail.com>
2021-05-09 16:48:27 +0300

    embed tx manager inside tx struct to proxy calls

--- pkg/core/transaction/tx_mgr.go
+++ pkg/core/transaction/tx_mgr.go
@@ -18,13 +18,13 @@ const (
 
 // Tx is a single-thread ACID transaction
 type Tx struct {
+	*TxManager
 	id       int
 	lockMode int
 	status   int
 	// lockedPages is a set of pages positions
 	// TODO: use regular map
 	lockedPages sync.Map
-	txMgr       *TxManager
 }
 
 func (t *Tx) Id() int {
@@ -68,7 +68,7 @@ func (m *TxManager) InitTxWithId(id int, lockMode int) *Tx {
 	tx := new(Tx)
 	tx.id = id
 	tx.lockMode = lockMode
-	tx.txMgr = m
+	tx.TxManager = m
 	return tx
 }
 
@@ -80,89 +80,89 @@ func (tx *Tx) validateTxStatus() {
 
 func (tx *Tx) fetchAndLockPage(pos int64) {
 	if _, found := tx.lockedPages.Load(pos); found {
-		tx.txMgr.sharedLockTable.UpgradeLock(pos, tx.id)
+		tx.sharedLockTable.UpgradeLock(pos, tx.id)
 		return
 	}
-	tx.txMgr.bufSlotMgr.Fetch(pos)
-	tx.txMgr.bufSlotMgr.Pin(pos)
-	tx.txMgr.sharedLockTable.Lock(pos, tx.lockMode)
-	tx.txMgr.sharedLockTable.UpgradeLock(pos, tx.id)
+	tx.bufSlotMgr.Fetch(pos)
+	tx.bufSlotMgr.Pin(pos)
+	tx.sharedLockTable.Lock(pos, tx.lockMode)
+	tx.sharedLockTable.UpgradeLock(pos, tx.id)
 	tx.lockedPages.Store(pos, struct{}{})
 }
 
 func (tx *Tx) DowngradeLocks() {
 	tx.lockedPages.Range(func(pos, _ interface{}) bool {
-		tx.txMgr.sharedLockTable.DowngradeLock(pos.(int64))
+		tx.sharedLockTable.DowngradeLock(pos.(int64))
 		return true
 	})
 }
 
 func (tx *Tx) AllocatePage() *storage.HeapPage {
-	return tx.txMgr.a.AllocatePage()
+	return tx.a.AllocatePage()
 }
 
 func (tx *Tx) ReadPageAtPos(pos int64) *storage.HeapPage {
 	tx.validateTxStatus()
 	tx.fetchAndLockPage(pos)
-	return tx.txMgr.bufSlotMgr.ReadPageAtPos(pos)
+	return tx.bufSlotMgr.ReadPageAtPos(pos)
 }
 
 func (tx *Tx) WritePageAtPos(page *storage.HeapPage, pos int64) {
 	tx.validateTxStatus()
 	tx.fetchAndLockPage(pos)
-	tx.txMgr.bufSlotMgr.WritePageAtPos(page, pos)
+	tx.bufSlotMgr.WritePageAtPos(page, pos)
 }
 
 func (tx *Tx) WritePage(page *storage.HeapPage) int64 {
 	tx.validateTxStatus()
-	pos := tx.txMgr.strgMgr.Extend()
+	pos := tx.strgMgr.Extend()
 	tx.WritePageAtPos(page, pos)
 	return pos
 }
 
 func (tx *Tx) CommitNoLog() {
 	tx.lockedPages.Range(func(pos, _ interface{}) bool {
-		tx.txMgr.bufSlotMgr.Flush(pos.(int64))
-		tx.txMgr.bufSlotMgr.Unpin(pos.(int64))
-		tx.txMgr.sharedLockTable.Unlock(pos.(int64))
+		tx.bufSlotMgr.Flush(pos.(int64))
+		tx.bufSlotMgr.Unpin(pos.(int64))
+		tx.sharedLockTable.Unlock(pos.(int64))
 		return true
 	})
-	tx.txMgr.strgMgr.Flush()
+	tx.strgMgr.Flush()
 }
 
 func (tx *Tx) AbortNoLog() {
 	tx.lockedPages.Range(func(pos, _ interface{}) bool {
-		tx.txMgr.bufSlotMgr.Flush(pos.(int64))
-		tx.txMgr.bufSlotMgr.Deallocate(pos.(int64))
-		tx.txMgr.sharedLockTable.Unlock(pos.(int64))
+		tx.bufSlotMgr.Flush(pos.(int64))
+		tx.bufSlotMgr.Deallocate(pos.(int64))
+		tx.sharedLockTable.Unlock(pos.(int64))
 		return true
 	})
 }
 
 func (tx *Tx) Commit() {
 	tx.lockedPages.Range(func(pos, _ interface{}) bool {
-		if page := tx.txMgr.bufSlotMgr.ReadPageIfDirty(pos.(int64)); page != nil {
+		if page := tx.bufSlotMgr.ReadPageIfDirty(pos.(int64)); page != nil {
 			snapshot, err := page.MarshalBinary()
 			if err != nil {
 				log.Panic(err)
 			}
-			tx.txMgr.logMgr.LogSnapshot(tx.id, pos.(int64), snapshot)
+			tx.logMgr.LogSnapshot(tx.id, pos.(int64), snapshot)
 		}
 		return true
 	})
-	tx.txMgr.logMgr.LogCommit(tx.id)
-	tx.txMgr.logMgr.Flush()
+	tx.logMgr.LogCommit(tx.id)
+	tx.logMgr.Flush()
 	tx.CommitNoLog()
 	tx.status = committed
 }
 
 func (tx *Tx) Abort() {
-	tx.txMgr.logMgr.LogAbort(tx.id)
-	tx.txMgr.logMgr.Flush()
+	tx.logMgr.LogAbort(tx.id)
+	tx.logMgr.Flush()
 	tx.AbortNoLog()
 	tx.status = aborted
 }
 
 func (tx *Tx) NoDataFound() bool {
-	return tx.txMgr.strgMgr.Empty()
+	return tx.strgMgr.Empty()
 }
