a09b28f0feadfb58a7bea441179d6e2c6cced90a
Mikhail Klyushin <klyuhsinmisha@gmail.com>
2021-05-30 20:00:37 +0300

    add fixups for dumb tests client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,93 +1,43 @@
 package main
 
-/*
-type DBMSRepl struct {
-
-}
-
-type DBMSClient struct {
-	conn net.Conn
-	// NewDumbSingleLineParser()
-	parser Parser
-	tx *ClientTx
-}
-
-func (c *DBMSClient) Exec(cmd string) error {
-	// send cmd to conn
-}
-
-func (c *DBMSClient) BeginTx() *ClientTx {
-	// send cmd to conn
-}
-
-const (
-	TxStateRunning = 0
-	TxStateFinished = 1
-)
-
-var (
-	ErrTxFinished = errors.New("tx processing finished")
-)
-
-type ClientTx struct {
-	state int
-}
-
-func (tx *ClientTx) Exec(cmd string) *transfer.Result {
-	if tx.state == TxStateFinished {
-		return transfer.ErrResult(ErrTxFinished)
-	}
-	if _, parseErr := tx.c.parser.Parse(strings.TrimSpace(cmd)); parseErr != nil {
-		return transfer.ErrResult(parseErr)
-	}
-	return tx.exec(cmd)
-}
-
-func (tx *ClientTx) Commit() {
-	if tx.state != TxStateFinished {
-		tx.commit()
-	}
-	return transfer.OkResult()
-}
-
-func (tx *ClientTx) Abort() {
-	if tx.state != TxStateFinished {
-		tx.abort()
-	}
-	return transfer.OkResult()
-}
-
-func main() {
-	client := NewDBMSClient(host, port)
-	res = client.Exec("GET key")
-	if res.err != nil {
-		...
-	}
-	tx := client.BeginTx()
-	defer tx.Commit()
-	tx.Exec("SET key value")
-	tx.
-}
-*/
-
 import (
 	"os"
 	"bufio"
 	"fmt"
 	"dbms/pkg/client"
-	"log"
 	"io"
 	"dbms/internal/transfer"
+	"flag"
+	"dbms/pkg"
+	"dbms/internal/parser"
 )
+
+var (
+	host string
+	port uint
+)
+
+
+func init() {
+	flag.StringVar(&host, "host", "localhost", "DBMS's hostname")
+	flag.UintVar(&port, "port", 8080, "DBMS's TCP-port")
+}
 
 // simple REPL for manual tests
 func main() {
-	dbClient, err := client.Connect("localhost:8080")
-	defer dbClient.Finalize()
+	flag.Parse()
+	dbClient, err := client.Connect(fmt.Sprintf("%s:%d", host, port))
 	if err != nil {
-		log.Panic(err)
+		fmt.Println(err)
+		return
 	}
+	defer dbClient.Finalize()
 	reader := bufio.NewReader(os.Stdin)
+	// TODO: maybe check major version for server before REPL starts?
+	fmt.Printf(`DBMS (version %s)
+Server: %s
+Port: %d
+`, pkg.Version, host, port)
 	for {
 		var msg string
 		fmt.Print("> ")
@@ -95,8 +45,12 @@ func main() {
 		res, err := dbClient.Exec(rawStrCmd)
 		if err == io.EOF {
 			return
+		} else if err == parser.ErrInvalidCmdStruct {
+			fmt.Println(err)
+			continue
 		} else if err != nil {
-			log.Panic(err)
+			fmt.Println(err)
+			return
 		}
 		switch res.Type() {
 		case transfer.OkResultCode:
