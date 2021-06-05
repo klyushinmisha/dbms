2f3603859d5a7ad68dc1eb3773ad7e1418ef658b
Mikhail Klyushin <klyuhsinmisha@gmail.com>
2021-06-05 19:49:28 +0300

    change spaces indent

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -68,15 +68,15 @@ func (c *DBMSClient) Exec(rawCmd string) (*transfer.Result, error) {
 		return nil, err
 	} else if cmd.Type == parser.HelpCmd {
 		return transfer.ValueResult([]byte(`Commands structure:
-Data manipulation commands:
-	GET key         - finds value associated with key
-	SET key value   - sets value associated with key
-	DEL key         - removes value associated with key
-Transaction management commands:
-	BEGIN SHARED    - starts new transaction with per-operation isolation
-	BEGIN EXCLUSIVE - starts new transaction with per-transation isolation
-	COMMIT          - commits active transaction
-	ABORT           - aborts active transaction`),
+  Data manipulation commands:
+    GET key         - finds value associated with key
+    SET key value   - sets value associated with key
+    DEL key         - removes value associated with key
+  Transaction management commands:
+    BEGIN SHARED    - starts new transaction with per-operation isolation
+    BEGIN EXCLUSIVE - starts new transaction with per-transation isolation
+    COMMIT          - commits active transaction
+    ABORT           - aborts active transaction`),
 		), nil
 	}
 	c.writer.WriteString(rawCmd)
